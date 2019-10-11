cde71b28acd49d0b2e7e9639790ee9d329fe2ab1
Ryan Matthews <ryan.matthews.professional@gmail.com>
2019-10-11 17:52:13 -0400

    implement barebones backend and auth

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express');
+require("dotenv").config();
 const db = require('./knexfile');
 const helmet = require('helmet');
-const lodash = require('lodash');
-
+const cors = require('cors')
 const PORT = process.env.PORT || 5000;
 const server = express();
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 
-server.use(express.json(), cors, helmet, lodash);
-
-// endpoints
+server.use(express.json(), cors(), helmet());
 
 server.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+  console.log(`\n=== Web API Listening on http://localhost:${PORT} ===\n`)
+});
+
+const authConfig = {
+  domain: process.env.domain,
+  audience: process.env.apiID
+};
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ["RS256"]
+});
+
+
+server.get("/api/external", checkJwt, (req, res) => {
+  res.status(200).json({message: "You have been authenticated!"})
 });
--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,19 @@
+require("dotenv").config();
+
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/notes.sqlite3'
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
     },
-    useNullAsDefault: true,
     migrations: {
-      directory: "./data/migrations"
+      directory: './data/migrations'
     },
     seeds: {
-      directory: "./data/seeds"
+      directory: './data/seeds/'
     }
   },
 
